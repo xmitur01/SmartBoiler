276f2eabc998b908a07156f689841938356f18d9
Jack <xmitur01@vubr.cz>
2020-12-13 16:32:10 +0100

    container setup

---
+++ start_containers.sh
@@ -0,0 +1,13 @@
+sudo docker run -it -p 1883:1883 -p 9001:9001 eclipse-mosquitto
+
+sudo docker run -d -p 8086:8086 -v influxdb:/var/lib/influxdb --name influxdb influxdb
+
+# create database in container
+# sudo docker exec -it influxdb influx
+#     create database sensors
+#     create user telegraf with password 'telegraf'
+#     grant all on sensors to telegraf
+
+sudo docker run -v $PWD/docker/telegraf.conf:/etc/telegraf/telegraf.conf:ro telegraf
+
+sudo docker run -d --name=grafana -p 3000:3000 grafana/grafana
