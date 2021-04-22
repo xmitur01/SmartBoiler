9fec27bba17038a3573248145aece413d46f40e4
Jb_ <71877384+xmitur01@users.noreply.github.com>
2021-04-22 17:19:28 +0200

    Update setupNewPlug.py

--- HS110/setupNewPlug.py
+++ HS110/setupNewPlug.py
@@ -15,7 +15,6 @@ plug.wifi_scan()
 
 SSID = input("Enter your network SSID: ")
 PASSWORD = input("Enter PASSWORD to your network: ")
-# KEY_TYPE = input("Enter network key type, from scan result: ")
 
-plug.wifi_join(SSID, PASSWORD)
 plug.erase_emeter_stats()
+plug.wifi_join(SSID, PASSWORD)
